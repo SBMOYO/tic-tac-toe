e696e71151277092d3ad0e0bbd831c650097a797
SBMOYO <125678676+SBMOYO@users.noreply.github.com>
2023-07-02 18:42:36 +0200

    implemented afunction that displays the instructions to the user

---
+++ instuction.py
@@ -0,0 +1,8 @@
+def main():
+    first_user = input("Player 1 name: ")
+    second_user = input("Player 2 name: ")
+    print(f"{first_user} has been assigned a symbol (X) and the {second_user} has been assigned a symbol (O) ")
+    print(f"The game will be played between {first_user} and {second_user}")
+    print("the objective is to make a straight line of three using your assigned symbols by choosing correct numbers and win!")
+
+main()
