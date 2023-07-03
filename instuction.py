8521c38c2ba10523ede731c802b194bbf194f9ec
SBMOYO <sunganomoyo1@gmail.com>
2023-07-03 16:18:12 +0200

    modified the print statements to make them one

--- instuction.py
+++ instuction.py
@@ -1,8 +1,10 @@
 def main():
     first_user = input("Player 1 name: ")
     second_user = input("Player 2 name: ")
-    print(f"{first_user} has been assigned a symbol (X) and the {second_user} has been assigned a symbol (O) ")
-    print(f"The game will be played between {first_user} and {second_user}")
-    print("the objective is to make a straight line of three using your assigned symbols by choosing correct numbers and win!")
+    print(f"""{first_user} has been assigned symbol (X) and the {second_user} has been assigned symbol (O) 
+The game will be played between {first_user} and {second_user}
+the objective is to make a straight line of three using your assigned symbols by choosing correct numbers and win!""")
 
-main()
+
+if __name__ == "__main__":
+    main()
